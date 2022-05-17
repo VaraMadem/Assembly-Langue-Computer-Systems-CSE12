2e44a326fdc0209f39ec68adac2802ba638cafa5
Vara Neha Madem <varamadem@college10-100-64-58-4.resnet.ucsc.edu>
2022-05-16 20:21:54 -0700

    adding files

---
+++ Lab3/Lab3.asm
@@ -0,0 +1,108 @@
+##########################################################################
+# Created by: Madem, Vara Neha
+#		vmadem
+#		May 16 2022
+#
+# Assignment: Lab 3 : S22
+#		CSE 12 Computer Systems and Assembly Language
+#		UC Santa Cruz, Spring 2022
+#
+
+
+# Description: This program prints a pattern using nested loops
+#
+# Notes: This program is intended to be run from the MARS IDE.
+##########################################################################
+
+.macro print_str(%string1) #macro to print any string
+	li a7,4 
+	la a0, %string1
+	ecall
+	.end_macro
+
+
+.data 				
+
+
+	prompt1: .asciz  "Enter the height of the pattern (must be greater than 0):"
+	errorMsg: .asciz  "Invalid Entry!"
+	break: .asciz  "\n"  
+
+.text 	
+	
+main: 	
+	j userinput	
+error:
+	print_str(errorMsg)		
+
+userinput:
+#prints prompt
+	li a7,4 
+	la a0, prompt1
+	ecall
+	
+	
+	li a7, 5
+	ecall
+	
+	
+	addi t0, a0, 0
+	
+	#error if height is 0 or less than 0
+	ble t0, zero, error
+	
+
+	
+	
+	
+
+li t1, 0
+li, t2, 1
+
+loop_1: #dollar signs and odd numbers
+	beqz t1, skip1
+	addi t2, t2, 2
+	
+mv t3, t1
+loop_2: #dollar signs and astericks
+
+
+	#dollar sign
+	li a7, 11
+	li a0, 36
+	ecall
+	#asterick
+	li a7, 11
+	li a0, 42
+	ecall
+	
+	addi t3, t3,-1
+	bgtz t3, loop_2
+
+skip1:
+li a7, 11
+li a0, 36
+ecall
+
+li, a7, 1
+mv, a0, t2
+ecall
+
+li a7, 4
+la a0, break
+ecall
+
+addi t1, t1, 1
+blt t1, t0, loop_1
+
+#exit
+exit:
+li a7, 10
+ecall
+
+
+
+
+	
+	
+
---
+++ Lab3/README.txt
@@ -0,0 +1,13 @@
+Vara Neha Madem 1872442
+Spring 2022
+Lab 3: Looping in RISC-V Assembly
+
+● List of all files in the directory (except for the README):
+add.asm
+firstRARSprogram.asm
+multiply.asm
+rars_533d3c0copy.jar
+
+
+● Instructions on how to run any code or simulations:
+Press assemble and then run
